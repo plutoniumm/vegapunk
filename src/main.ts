65debac9f76c2db3551e05fe1d8e92ba21300579
plutoniumm <51614345+plutoniumm@users.noreply.github.com>
2026-01-14 21:40:11 +0530

    reader test

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,21 @@
+import { Readability } from "@mozilla/readability";
 import App from './App.svelte';
 import { mount } from 'svelte';
 
 
 let frameText = '';
 if (window.self !== window.top) {
-  let text = JSON.parse(window.name).text;
-  text = decodeURIComponent(text);
+  let text = decodeURIComponent(window.name);
+
+  const document = new DOMParser().parseFromString(text, 'text/html');
+  const article = new Readability(document).parse();
+
+  if (article?.textContent) {
+    text = article.textContent.trim();
+  } else {
+    text = document.body.innerText;
+  };
+
   frameText = text;
 }
 
